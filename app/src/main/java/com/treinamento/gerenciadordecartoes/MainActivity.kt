d66d7da4f230f3fd7796a925700c9c22e332ad4f
Rogério Ricardo Nunes <rogerio.r.nunes@bradesco.com.br>
2026-08-18 15:47:17 -0300

    Subindo themas e android manifest

--- app/src/main/java/com/treinamento/gerenciadordecartoes/MainActivity.kt
+++ app/src/main/java/com/treinamento/gerenciadordecartoes/MainActivity.kt
@@ -1,47 +1,14 @@
-package com.treinamento.gerenciadordecartoes
+package com.example.cardmanager
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.treinamento.gerenciadordecartoes.ui.theme.GerenciadorDeCartoesTheme
+import com.example.cardmanager.navigation.CardManagerApp
+import com.example.cardmanager.ui.theme.CardManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            GerenciadorDeCartoesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GerenciadorDeCartoesTheme {
-        Greeting("Android")
+        setContent { CardManagerTheme { CardManagerApp() } }
     }
 }
